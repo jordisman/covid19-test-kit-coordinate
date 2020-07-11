37ffeaf4c07e493f648d4e7716159549061f784a
Jordis Man <jordisman@gmail.com>
2020-07-11 13:41:16 -0700

    Completed Landing Page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,17 +8,32 @@ const LandingPage = () => (
       <img src={Illustration01} className={css.illustration} alt="" />
 
       <div className={css.textBox}>
-        <h2>Healthcare Providers</h2>
+        <h1>Healthcare Providers</h1>
         <p className={css.text}>
           Test Line Covid-19 provides an easy tool for test kit resources
           sharing and triaging between clinics, test centers, and hospitals.
         </p>
-
         <div className={css.signUpProvBtn}>Sign Up as Provider</div>
         <p>
           Not a Healthcare Provider? <span>Sign Up Here</span>
         </p>
+      </div>
 
+      <div className={css.textBox2}>
+        <h1>What We Do</h1>
+        <p>
+          Times are tough, but we believe with the right kind of collaboration,
+          we take one step forward to rebuilding the health of our nation.
+        </p>
+        <h5>Features:</h5>
+        <p>
+          - Report test-kit info via a form <br/>
+          - Organize clinic/test-kit availability onto a map
+        </p>
+        <div className={css.signUpProvBtn}>Sign Up as Provider</div>
+        <p>
+          Not a Healthcare Provider? <span>Sign Up Here</span>
+        </p>
       </div>
     </div>
   </div>
