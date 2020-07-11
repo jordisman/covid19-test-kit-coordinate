8d5b4646ac545a4aaa3b86bdf30cde52259bb8a2
Jordis Man <jordisman@gmail.com>
2020-07-11 14:19:00 -0700

    Edited Landing Page, added mini form

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Illustration01 from '../images/Illustration01.png';
+import miniForm from '../images/miniForm.png';
 import css from '../styles/styles.module.css';
 
 const LandingPage = () => (
@@ -8,7 +9,7 @@ const LandingPage = () => (
       <img src={Illustration01} className={css.illustration} alt="" />
 
       <div className={css.textBox}>
-        <h1>Healthcare Providers</h1>
+        <p className={css.title}>Healthcare Providers</p>
         <p className={css.text}>
           Test Line Covid-19 provides an easy tool for test kit resources
           sharing and triaging between clinics, test centers, and hospitals.
@@ -20,12 +21,12 @@ const LandingPage = () => (
       </div>
 
       <div className={css.textBox2}>
-        <h1>What We Do</h1>
+        <p className={css.title}>What We Do</p>
         <p>
           Times are tough, but we believe with the right kind of collaboration,
           we take one step forward to rebuilding the health of our nation.
         </p>
-        <h5>Features:</h5>
+        <p><b>Features:</b></p>
         <p>
           - Report test-kit info via a form <br/>
           - Organize clinic/test-kit availability onto a map
@@ -34,6 +35,10 @@ const LandingPage = () => (
         <p>
           Not a Healthcare Provider? <span>Sign Up Here</span>
         </p>
+      </div>
+
+      <div>
+        <img src={miniForm} className={css.miniForm} />
       </div>
     </div>
   </div>
